b1da1e1e0d514864a5c7d8beb25113c6da206f0e
Mika Vormisto <mika.vormisto@outlook.com>
2018-04-11 09:06:09 +0300

    Moved the log files to a service

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LogService } from '../../services/log.service';
+
 import { Log } from '../../models/log';
 
 @Component({
@@ -10,29 +12,9 @@ import { Log } from '../../models/log';
 export class LogsComponent implements OnInit {
   logs: Log[];
 
-  constructor() { }
+  constructor(private logService: LogService) { }
 
   ngOnInit() {
-    this.logs = [
-      {
-        id: '1',
-        title: 'Generated components',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, dolor?',
-        date: new Date ('12/26/2017 12:54:23')
-      },
-      {
-        id: '2',
-        title: 'Generated UI',
-        // tslint:disable-next-line:max-line-length
-        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, ut? Animi modi dolore amet aspernatur quas, labore quibusdam ab fuga.',
-        date: new Date ('12/27/2017 12:54:23')
-      },
-      {
-        id: '3',
-        title: 'Added logs component',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-        date: new Date ('12/28/2017 12:54:23')
-      }
-    ];
+    this.logs = this.logService.getLogs();
   }
 }
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@angular/core';
 
+import { Log } from '../models/log';
+
 @Injectable()
 export class LogService {
 
-  constructor() { }
+  logs: Log[];
+
+  constructor() {
+    this.logs = [
+      {
+        id: '1',
+        title: 'Generated components',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, dolor?',
+        date: new Date ('12/26/2017 12:54:23')
+      },
+      {
+        id: '2',
+        title: 'Generated UI',
+        // tslint:disable-next-line:max-line-length
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, ut? Animi modi dolore amet aspernatur quas, labore quibusdam ab fuga.',
+        date: new Date ('12/27/2017 12:54:23')
+      },
+      {
+        id: '3',
+        title: 'Added logs component',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        date: new Date ('12/28/2017 12:54:23')
+      }
+    ];
+  }
+
+  getLogs() {
+    return this.logs;
+  }
 
 }
